d680e40b4bddfd702566cced55e20ad09840b7d8
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-04 08:56:45 -0300

    Getting information from "loadGames()" by using "dispatch"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { loadGames } from "./actions/gamesAction"
 
 function App() {
+  const dispatch = useDispatch()
+
+  // As soon as the "App" component renders, we will "dispatch" loadGames() information:
+  useEffect(() => {
+    dispatch(loadGames())
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Ignite</h1>
     </div>
   );
 }
