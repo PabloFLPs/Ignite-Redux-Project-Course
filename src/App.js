9597a9ef082cc46e0892c8c315a2eaec212d265b
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-05 05:42:40 -0300

    Switching "fetch games" logic to Home component and importing this component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { useEffect } from "react"
-import { useDispatch } from "react-redux"
-import { loadGames } from "./actions/gamesAction"
+import React from "react"
+
+// Components and Pages:
+import Home from "./pages/Home"
 
 function App() {
-  const dispatch = useDispatch()
-
-  // As soon as the "App" component renders, we will "dispatch" loadGames() information:
-  useEffect(() => {
-    dispatch(loadGames())
-  })
-
   return (
     <div className="App">
       <h1>Hello Ignite</h1>
+      <Home />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
