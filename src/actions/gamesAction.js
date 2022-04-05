6d3a9c5a282cdfa5b883cce0a3905f03cfd7e0ec
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-05 05:31:40 -0300

    Adding more fetching payloads and comments

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,14 +1,20 @@
+// An "action" is basically an object that contains information.
 import axios from "axios"
-import { popularGamesURL } from "../api"
+import { newGamesURL, popularGamesURL, upcomingGamesURL } from "../api"
 
 // Action Creator:
+// "Action creators" are functions that creates the "actions".
 export const loadGames = () => async (dispatch) => {
     // Fetch witch Axios:
     const popularData = await axios.get(popularGamesURL())
+    const newData = await axios.get(newGamesURL())
+    const upcomingData = await axios.get(upcomingGamesURL())
     dispatch({
         type: "FETCH_GAMES",
         payload: {
-            popular: popularData,
+            popularGames: popularData.data.results,
+            newGames: newData.data.results,
+            upcomingGames: upcomingData.data.results,
         },
     })
 }
