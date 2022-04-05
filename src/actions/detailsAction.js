969075d258815464c9d99915010fbc1d0017fa9a
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-05 10:26:22 -0300

    Fixing "undefined" error, "id" parameter was not being passed before

--- src/actions/detailsAction.js
+++ src/actions/detailsAction.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { gameDetailsURL } from "../api"
 
 export const loadDetails = (id) => async (dispatch) => {
-    const detailsData = await axios.get(gameDetailsURL())
+    const detailsData = await axios.get(gameDetailsURL(id))
 
     dispatch({
         type: "GET_DETAILS",
