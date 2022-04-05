9416c254ad840fa36e12c3fec76fb5b55a0b258e
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-05 08:11:01 -0300

    Importing GlobalStyles component and adding it to App page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react"
 // Components and Pages:
 import Home from "./pages/Home"
 
+import GlobalStyles from "./components/GlobalStyles"
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Ignite</h1>
+      <GlobalStyles />
       <Home />
     </div>
   )
