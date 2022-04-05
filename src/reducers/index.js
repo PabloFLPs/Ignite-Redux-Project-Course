9f6fb7cab6a382efc3e1d3ecb689d4cecf90dc88
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-05 08:46:19 -0300

    Importing "detailsReducer" and adding it to the reducers root

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 // We will have multiple reducers, so:
 import { combineReducers } from "redux"
+
 import gamesReducer from "./gamesReducer"
+import detailsReducer from "./detailsReducer"
 
 const rootReducer = combineReducers({
     games: gamesReducer,
+    detail: detailsReducer
 })
 
 export default rootReducer
