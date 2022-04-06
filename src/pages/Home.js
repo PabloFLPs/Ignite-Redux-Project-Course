b99e8026dbe0fef50d4984784735d219bd46de4a
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-06 17:59:20 -0300

    Getting gameID fromt useLocation().pathname and adding GameDetails component to page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,10 +6,13 @@ import { loadGames } from "../actions/gamesAction"
 
 // Components:
 import Game from "../components/Game"
+import GameDetails from "../components/GameDetails"
 
 // Styling and Animation:
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
+
+import { useLocation } from "react-router-dom"
 
 const GameList = styled(motion.div)`
     padding: 0rem 5rem;
@@ -23,10 +26,13 @@ const Games = styled(motion.div)`
     grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
     grid-column-gap: 3rem;
     grid-row-gap: 5rem;
-
 `
 
 const Home = () => {
+    // Getting current "location":
+    const location = useLocation()
+    const pathID = location.pathname.split("/")[2]
+
     // Fetch Games:
     const dispatch = useDispatch()
 
@@ -36,11 +42,14 @@ const Home = () => {
     }, [dispatch])
 
     // Getting back the data:
-    const {popularGames, newGames, upcomingGames} = useSelector((state) => state.games)
-    //console.log(popularGames, newGames, upcomingGames)
+    const { popularGames, newGames, upcomingGames } = useSelector((state) => state.games)
+    console.log("Popular Games:", popularGames)
+    console.log("New Games:", newGames)
+    console.log("Upcoming Games", upcomingGames)
 
     return (
         <GameList>
+            {pathID && <GameDetails />}
             <h2>Upcoming Games</h2>
             <Games>
                 {upcomingGames.map((game) => (
