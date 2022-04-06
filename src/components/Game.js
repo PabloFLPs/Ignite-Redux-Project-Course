80ed088089d8e2157d4d40f7fdf9a7f72d7a8e17
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-06 17:29:39 -0300

    Adding <Link/> component to get gameID from URL params

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,11 +8,15 @@ import { motion } from 'framer-motion'
 import { useDispatch } from "react-redux"
 import { loadDetails } from "../actions/detailsAction"
 
+import { Link } from 'react-router-dom'
+
 const StyledGame = styled(motion.div)`
     min-height: 30vh;
     box-shadow: 0px 5px 20px rgba(0,0,0,0.4);
     text-align: center;
     border-radius: 0.4rem;
+    cursor: pointer;
+
     img {
         width: 100%;
         height: 30vh;
@@ -29,9 +33,11 @@ const Game = ({ id, name, image, released }) => {
 
     return (
         <StyledGame onClick={loadDetailsHandler}>
-            <h3>{name}</h3>
-            <p>{released}</p>
-            <img src={image} alt={name + " - Background Image"}/>
+            <Link to={`/game/${id}`}>
+                <h3>{name}</h3>
+                <p>{released}</p>
+                <img src={image} alt={name + " - Background Image"}/>
+            </Link>
         </StyledGame>
     )
 }
