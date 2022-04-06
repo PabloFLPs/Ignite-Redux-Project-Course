b6332222294f642bf4121f263e92f1c6aa20eed4
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-06 18:31:39 -0300

    Getting screenshotData (it was missing) and adding "loadDetails" action

--- src/actions/detailsAction.js
+++ src/actions/detailsAction.js
@@ -1,13 +1,19 @@
 import axios from "axios"
-import { gameDetailsURL } from "../api"
+import { gameDetailsURL, gameScreenshotURL } from "../api"
 
 export const loadDetails = (id) => async (dispatch) => {
+    dispatch({
+        type: "LOADING_DETAILS",
+    })
+
     const detailsData = await axios.get(gameDetailsURL(id))
+    const screenshotData = await axios.get(gameScreenshotURL(id))
 
     dispatch({
         type: "GET_DETAILS",
         payload: {
             game: detailsData.data,
+            screen: screenshotData.data,
         },
     })
 }
