95163e35779a1a838cf23be61831e542f9f61f69
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-06 18:36:13 -0300

    Adding "screen" state (it was missing) and creating "LOADING_DETAILS" reducer

--- src/reducers/detailsReducer.js
+++ src/reducers/detailsReducer.js
@@ -12,6 +12,13 @@ const detailsReducer = (state = initialState, action) => {
             return {
                 ...state,
                 game: action.payload.game,
+                screen: action.payload.screen,
+                isLoading: false,
+            }
+        case "LOADING_DETAILS":
+            return {
+                ...state,
+                isLoading: true,
             }
         default:
             return {...state}
