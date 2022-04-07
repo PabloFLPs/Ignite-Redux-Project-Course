43ea2f57c012adc52d52995e8c010642cdc59a61
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-07 06:38:03 -0300

    Adding NavBar component to our App

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react"
 
 // Components and Pages:
 import Home from "./pages/Home"
+import NavBar from "./components/NavBar"
 
+// Styles:
 import GlobalStyles from "./components/GlobalStyles"
 
+// Routing:
 import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <NavBar />
       <Routes>
         <Route path={"game/:id"} element={<Home />} />
         <Route path={"/"} element={<Home />} />
